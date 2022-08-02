7f343ee1666c4df6b8b89bc731e15be55228befa
Mario Esteban Mateo <memateo@gmail.com>
2022-08-02 16:14:46 -0300

    2nd commit

--- routes/router.js
+++ routes/router.js
@@ -1,16 +1,16 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index")
-})
+  res.render("index");
+});
 
 router.get("/login", (req, res) => {
-    res.render("login")
-})
+  res.render("login");
+});
 
 router.get("/register", (req, res) => {
-    res.render("register")
-})
+  res.render("register");
+});
 
-module.exports = router
+module.exports = router;
